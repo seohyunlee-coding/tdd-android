999a65bed041e881397fb527b193d6e10c3a2494
Seohyun Lee <cwijiq3085@gmail.com>
2025-10-20 12:16:24 +0900

    실습1&실습2

---
+++ FourBasicOpt.py
@@ -0,0 +1,15 @@
+class FourBasicOpt:
+  """For the four basic arithmetic operations.
+Class can be used to add(addition), subtract(subtraction), divide(division) and multiply(multiplication).
+"""
+  def add(self, x, y):
+    return x + y
+
+  def subtract(self, x, y):
+    return x - y
+  
+  def divide(self, x, y):
+    return x / y
+
+  def multiply(self, x, y):
+    return x * y
